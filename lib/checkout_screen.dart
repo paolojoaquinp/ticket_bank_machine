0cea1457029e47fbdaf8271c4961e3d1d855908a
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2024-12-11 13:15:57 -0400

    feat: layout screen and horizontal assets cards

---
+++ lib/checkout_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class CheckoutScreen extends StatelessWidget {
+  const CheckoutScreen({super.key, required this.index,});
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          forceMaterialTransparency: true,
+        ),
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            children: [
+              Container(
+                width: double.maxFinite,
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade400, width: 1.0),
+                    borderRadius:
+                        const BorderRadius.all(Radius.circular(12.0))),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset("assets/credit-card-h-${index % 4}.png"),
+                    const SizedBox(height: 12.0,),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Enter a CVV Number'),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(6.0),
+                              ),
+                              color: Colors.grey[200],
+                            ),
+                            width: 120,
+                            height: 32,
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 24,),
+              Expanded(
+                child: _numericKeyPad(),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Widget _numericKeyPad() {
+    return Container(
+      color: Colors.white,
+      child: GridView.builder(
+        itemCount: 11,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 15.0,
+          crossAxisSpacing: 40.0,
+        ),
+        itemBuilder: (context, index) {
+          if (index == 9) {
+            return SizedBox.shrink();
+          } else if (index == 10) {
+            return InkWell(
+              child: CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.grey.shade300,
+                child: Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            );
+          } else {
+            // Botones del 1 al 9
+            return InkWell(
+              child: CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.grey.shade300,
+                child: Text(
+                  '${index + 1}',
+                  style: TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:ticket_bank_machine/checkout_screen.dart';
 import 'package:ticket_bank_machine/widgets/scrollable_card_stack.dart';
 import 'package:ticket_bank_machine/widgets/card_credit_ticket.dart';
 
@@ -13,54 +14,77 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ticket bank machine',
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(
-            'Payments',
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          backgroundColor: Colors.transparent,
-          forceMaterialTransparency: true,
-          centerTitle: false,
-          leading: GestureDetector(
-            child: Image(
-              image: AssetImage('assets/back-icon.png'),
-            ),
+      home: _HomeScreen(),
+    );
+  }
+}
+
+class _HomeScreen extends StatelessWidget {
+  const _HomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Payments',
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+        backgroundColor: Colors.transparent,
+        forceMaterialTransparency: true,
+        centerTitle: false,
+        leading: GestureDetector(
+          child: Image(
+            image: AssetImage('assets/back-icon.png'),
           ),
         ),
-        body: Expanded(
-          child: Column(
-            children: [
-              const Expanded(
-                flex: 1,
-                child: DetailsDashboard(),
-              ),
-              SizedBox(
-                height: MediaQuery.sizeOf(context).height * 0.05,
-              ),
-              Expanded(
-                flex: 2,
-                child: PerspectiveListView(
-                  visualizedItems: 4,
-                  itemExtent: MediaQuery.sizeOf(context).height * .48,
-                  initialIndex: 7,
-                  backItemsShadowColor:
-                      Theme.of(context).scaffoldBackgroundColor,
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  onTapFrontItem: (value) {
-                    
+      ),
+      body: Expanded(
+        child: Column(
+          children: [
+            const Expanded(
+              flex: 1,
+              child: DetailsDashboard(),
+            ),
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.05,
+            ),
+            Expanded(
+              flex: 2,
+              child: PerspectiveListView(
+                visualizedItems: 4,
+                itemExtent: MediaQuery.sizeOf(context).height * .48,
+                initialIndex: 7,
+                backItemsShadowColor:
+                    Theme.of(context).scaffoldBackgroundColor,
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                onTapFrontItem: (currentIndex) {
+                  Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) => CheckoutScreen(index: currentIndex ?? 0,),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        return FadeTransition(
+                          opacity: animation,
+                          child: child,
+                        );
+                      },
+                    ),
+                  );
+                },
+                children: List.generate(
+                  20,
+                  (index) {
+                    return CardCreditTicket(index: index);
                   },
-                  children: List.generate(
-                    20,
-                    (index) {
-                      return CardCreditTicket(index: index);
-                    },
-                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
